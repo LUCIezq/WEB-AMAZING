cdd370e0b574b05deff957d5918486f9e1997620
LUCIezq <lucieze02@icloud.com>
2026-09-14 11:55:36 -0300

    refactor: Cambio descripciones de cada integrante

--- src/data/Team.ts
+++ src/data/Team.ts
@@ -13,23 +13,23 @@ interface TeamInfo {
 export const teamInfo: TeamInfo[] = [
   {
     id: "marcela",
-    nombre: "Marcela",
+    nombre: "Marce",
     descripcion:
-      "psum dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et. dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et. dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et.",
+      "Los grandes viajes comienzan con un sueño. En Amazing Travel, nos encanta transformar ese sueño en un viaje pensado hasta el último detalle, combinando experiencia, planificación y mucha magia.Para mí, viajar no es solo conocer un destino: es hacer realidad un sueño y crear recuerdos que te acompañen toda la vida.",
     imagen: marcela,
   },
   {
     id: "magali",
     nombre: "Majo",
     descripcion:
-      "psum dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et. dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et. dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et.",
+      "Mi pasión por viajar y mi experiencia me enseñaron que cada viaje es único. En Amazing Travel, transformamos sueños en experiencias, acompañando cada detalle para que solo tengas que ocuparte de disfrutar.",
     imagen: majo,
   },
   {
     id: "cecilia",
-    nombre: "Cecilia",
+    nombre: "Ceci",
     descripcion:
-      "psum dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et. dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et. dolor sit amet consectetur adipisicing elit. Saepe, aperiam reprehenderit eum quis amet et.",
+      "Soy de las que creen que un viaje no se compra: se sueña, se diseña y se vive. Con más de 8 años de experiencia en turismo, mi especialidad es convertir cada viaje en una experiencia personalizada, cuidada y, sobre todo, inolvidable.",
     imagen: cecilia,
   },
 ];
